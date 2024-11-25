e62ab1028cfa39a2e18f03adf19136766b1204b6
aryajason <aryajasonr@gmail.com>
2024-11-25 14:13:24 +0700

    Update main.cpp

--- 4.Find Maximum in a List/main.cpp
+++ 4.Find Maximum in a List/main.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 int main()
 {
-    int number[] = { 12,2,4,5,6,7,90,23,10} ;
+    int number[] = {12,2,4,5,6,7,90,23,10} ;
     int length = 10 ;
-    int maximum = 0;
+    int maximum = 0 ;
     
     for(int i=0;i<length;i++)
     {
