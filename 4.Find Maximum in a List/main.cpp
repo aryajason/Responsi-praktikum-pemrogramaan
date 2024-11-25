65c0ed491344eea16cd6682d24beca45e0bb414e
aryajason <aryajasonr@gmail.com>
2024-11-25 14:05:25 +0700

    Update main.cpp

--- 4.Find Maximum in a List/main.cpp
+++ 4.Find Maximum in a List/main.cpp
@@ -2,16 +2,16 @@
 using namespace std;
 int main()
 {
-    int array[] = { 12,2,4,5,6,7,90,23,10} ;
+    int number[] = { 12,2,4,5,6,7,90,23,10} ;
     int length = 10 ;
     int maximum ;
     
     for(int i=0;i<length;i++)
     {
-        if (maximum<array[i])
+        if (maximum<number[i])
         {
-            maximum = array[i] ;
+            maximum = number[i] ;
         }
     }
-    cout << "the maximum number for this list is: " << maximum ;
+    cout << "the maximum number in this list is: " << maximum ;
 }
